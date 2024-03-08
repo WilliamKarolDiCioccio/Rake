d763c770e7e3d390e9cc71ccde8ed56fcc116ed2
Wilielmus <88447902+WilliamKarolDiCioccio@users.noreply.github.com>
2024-03-09 00:01:56 +0100

    Fix command line option handling (value lists)

--- Engine/Runtime/include/metadata.hpp
+++ Engine/Runtime/include/metadata.hpp
@@ -3,9 +3,9 @@
 #define PROJECT_NAME "RakeEngine"
 #define PROJECT_VERSION "0.0"
 #define LOCAL_BUILD_DATE "2024-03-08"
-#define LOCAL_BUILD_TIME "20:59:54"
+#define LOCAL_BUILD_TIME "23:56:47"
 #define GMT_BUILD_DATE "2024-03-08"
-#define GMT_BUILD_TIME "19:59:54"
+#define GMT_BUILD_TIME "22:56:47"
 
 // Metadata generated in local timezone: Etc/GMT-1
 // Good work fellow programmer!
